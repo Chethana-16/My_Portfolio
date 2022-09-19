d7842f0e0a7c0c8e21555de8f3a9acb64cc9793e
Chethana-16 <chethana1681@gmail.com>
2022-09-19 14:52:26 +0530

    second commit

--- src/About.js
+++ src/About.js
@@ -7,11 +7,13 @@ class About extends Component {
             <div className="about">
                 <h2>Who am i?</h2>
                 <div className="border"></div>
-                <p>I am a Computer Science student at DSATM, bangalore. I love learning new things besides working on some projects of my intrest in my free time.</p>
+                <p className="p">I am a Computer Science student at DSATM, bangalore. I love learning new things besides working on some projects of my intrest in my free time.I also enjoy listening music, dancing or writing stories and poetry sometimes.Ever since I got to know more about data science and analytics and powerful technologies such as python, Power BI, etc. I got more curious and got addicted to gaining more and more knowledge in this field. I've attended many workshops, and webinars
+                 and I often see myself working on some python or power BI-based projects or practicing my coding skills on websites that are dedicated to coding practices.Though I'm still
+                 a novice in this field, I have always been confident about the precious knowledge I own.I strongly belive that my tireless efforts to aid my love and passion for data analysis will someday become a solid
+                 platform for my growth.</p>
             </div>
 
         )
-    }
+    }   
 }
 export default About;
-    
